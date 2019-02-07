627e0bcb5df5882b272681a94dfed63909e64acb
Josh Smith <joshuap.smith12@gmail.com>
2019-02-06 21:39:44 -0600

    Added search to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,26 +4,66 @@ import {
   Container,
   Button,
   Content,
-  Text } from 'native-base';
+  Text,
+  Input,
+  Item,
+  Label
+} from 'native-base';
 
 export default class HomeScreen extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calculators: [
+        {
+          name: 'Vancomycin Empiric Dosing',
+          url: 'VancomycinEmpiricDosing'
+        },
+        {
+          name: 'Creatine Clearance',
+          url: 'CreatineClearance'
+        }
+      ],
+      searchString: ''
+    };
+  }
+
   public render() {
     return (
       <Container>
-        <AppHeader title="Home"/>
+        <AppHeader title="Home" />
+        <Item rounded>
+        <Label>Search: </Label>
+        <Input
+          value={this.state.searchString}
+          onChangeText={this.handleSearchChange}
+        />
+        </Item>
         <Content padder>
-          <Button full rounded dark
-            style={{ marginTop: 10 }}
-            onPress={() => this.props.navigation.navigate('VancomycinEmpiricDosing')}>
-            <Text>Vancomycin Empiric Dosing</Text>
-          </Button>
-          <Button full rounded dark
-            style={{ marginTop: 10 }}
-            onPress={() => this.props.navigation.navigate('CreatineClearance')}>
-            <Text>Creatine Clearance</Text>
-          </Button>
+          {this.calculatorList()}
         </Content>
       </Container>
     );
+  }
+
+  private handleSearchChange = (searchString: string) => {
+    this.setState({
+      ...this.state,
+      searchString: searchString
+    });
+  }
+  private calculatorList = () => {
+    return this.state.calculators.map((calculator, i) => {
+      if (calculator.name.toLowerCase().includes(this.state.searchString.toLowerCase())) {
+        return (
+          <Button full rounded dark
+            style={{ marginTop: 10 }}
+            key={i}
+            onPress={() => this.props.navigation.navigate(calculator.url)}>
+            <Text>{calculator.name}</Text>
+          </Button>
+        );
+      }
+    });
   }
 }
